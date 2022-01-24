f652ed81ec1de0f7209ac92b52c1a43a61d2d311
nikolausliu <18010855044@163.com>
2022-01-24 16:01:53 +0800

    feat: 菜单操作接口

--- routes/menus.js
+++ routes/menus.js
@@ -4,6 +4,15 @@ const util = require('../utils/util')
 
 router.prefix('/menus')
 
+router.get('/list', async (ctx) => {
+  const { menuName, menuState } = ctx.request.query
+  const params = {}
+  if (menuName) params.menuName = menuName
+  if (menuState) params.menuState = menuState
+  const rootList = await Menu.find(params) || []
+  ctx.body = util.success(rootList)
+})
+
 router.post('/operate', async (ctx) => {
   const { _id, action, ...params } = ctx.request.body
   console.log('=>', params)
@@ -13,12 +22,18 @@ router.post('/operate', async (ctx) => {
       res = await Menu.create(params)
       info = '创建成功'
     } else if (action === 'edit') {
+      params.updateTime = new Date()
+      res = await Menu.findByIdAndUpdate(_id, params)
       info = '编辑成功'
     } else {
+      res = await Menu.findByIdAndRemove(_id)
+      await Menu.deleteMany({ parentId: { $all: [_id] } })
       info = '删除成功'
     }
-  } catch (error) { }
-  ctx.body = util.success('', info)
+    ctx.body = util.success('', info)
+  } catch (error) {
+    ctx.body = util.fail('', `操作失败: ${error.message}`)
+  }
 })
 
 module.exports = router
